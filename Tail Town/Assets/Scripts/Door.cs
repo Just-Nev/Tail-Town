9687d9ec97675637822e11e7f84b77fa5d63f456
Just-Nev <cperry683@gmail.com>
2025-07-17 16:45:25 +0100

    Door Script

--- Tail Town/Assets/Scripts/Door.cs
+++ Tail Town/Assets/Scripts/Door.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float rotationSpeed = 180f; // Degrees per second
+    private bool isOpen = false;
+    private bool isRotating = false;
+
+    void OnMouseDown()
     {
-        
+        if (!isRotating)
+        {
+            float targetAngle = isOpen ? 0f : 90f;
+            StartCoroutine(RotateToAngle(targetAngle));
+            isOpen = !isOpen;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RotateToAngle(float targetZ)
     {
-        
+        isRotating = true;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, targetZ);
+
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.1f)
+        {
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+        isRotating = false;
     }
 }
+
